7f73bba6060e9bebca4dc38368f0699e273bb9d2
Yura Prisyazhnyy <cssuperpy@gmail.com>
2022-02-17 01:12:03 +0200

    Categories ready

---
+++ migrations/2022.02.16T22.42.45.create-category.ts
@@ -0,0 +1,15 @@
+import {Migration} from '../umzug';
+import {DataTypes, Sequelize} from 'sequelize';
+
+export const up: Migration = async ({context: sequelize}) => {
+  await sequelize.getQueryInterface().createTable('Categories', {
+    id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+    isDeleted: {type: DataTypes.BOOLEAN, defaultValue: false},
+    guid: {type: DataTypes.STRING, unique: true, defaultValue: Sequelize.fn('UUID')}
+  });
+};
+
+export const down: Migration = async ({context: sequelize}) => {
+  await sequelize.getQueryInterface().dropTable('Categories');
+};
---
+++ seeders/2022.02.16T22.43.37.seed-category.ts
@@ -0,0 +1,16 @@
+import {Migration} from '../umzug';
+
+const data = [
+  {id: 1, name: 'Специалист'},
+  {id: 2, name: 'Специалист 2 категории'},
+  {id: 3, name: 'Специалист 1 категории'},
+  {id: 4, name: 'Специалист высшей категории'}
+];
+
+export const up: Migration = async ({context: sequelize}) => {
+  await sequelize.getQueryInterface().bulkInsert('Categories', data);
+};
+
+export const down: Migration = async ({context: sequelize}) => {
+  await sequelize.getQueryInterface().bulkDelete('Categories', {});
+};
